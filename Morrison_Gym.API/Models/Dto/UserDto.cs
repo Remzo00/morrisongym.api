16e3d3168400fbd03af2b87bb11b068b9f0dcc3e
Remzo00 <gusinacremzo00@gmail.com>
2022-09-23 11:34:09 +0200

    Add id to dto models

--- Morrison_Gym.API/Models/Dto/UserDto.cs
+++ Morrison_Gym.API/Models/Dto/UserDto.cs
@@ -2,6 +2,7 @@
 {
     public class UserDto
     {
+        public int Id { get; set; }
         public Guid UserCode { get; set; }
         public string UserName { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
